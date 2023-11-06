78ee31a9af63ec45d436fdea64f0b7e8dfa63277
Pumelele <pumelele.qata01@gmail.com>
2023-11-06 22:43:24 +0200

    CRUD for categories

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,9 @@ import AdminDashboard from './pages/Admin/AdminDashboard';
 import CreateCategory from './pages/Admin/CreateCategory';
 import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/Users';
+import Orders from './pages/user/Orders';
+import Profile from './pages/user/Profile';
+import Products from './pages/Admin/Products';
 function App() {
   return (
     <>
@@ -22,12 +25,15 @@ function App() {
         <Route path='/' element={<HomePage/>} />
         <Route path='/dashboard' element={<PrivateRoute/>}>
           <Route path='user' element={<Dashboard/>} />
+          <Route path='user/orders' element={<Orders/>} />
+          <Route path='user/profile' element={<Profile/>} />
         </Route>
         <Route path='/dashboard' element={<AdminRoute/>}>
           <Route path='admin' element={<AdminDashboard/>} />
           <Route path='admin/create-category' element={<CreateCategory/>} />
           <Route path='admin/create-product' element={<CreateProduct/>} />
           <Route path='admin/users' element={<Users/>} />
+          <Route path="admin/products" element={<Products />} />
         </Route>
         <Route path='/register' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
