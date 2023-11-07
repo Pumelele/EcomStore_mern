c751918bb9fe224cf218fd942f6fea3aa0a884b5
Pumelele <pumelele.qata01@gmail.com>
2023-11-07 23:00:37 +0200

    Completed Admin menu and Product CRUD

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Users from './pages/Admin/Users';
 import Orders from './pages/user/Orders';
 import Profile from './pages/user/Profile';
 import Products from './pages/Admin/Products';
+import UpdateProduct from './pages/Admin/UpdateProduct';
 function App() {
   return (
     <>
@@ -34,6 +35,7 @@ function App() {
           <Route path='admin/create-product' element={<CreateProduct/>} />
           <Route path='admin/users' element={<Users/>} />
           <Route path="admin/products" element={<Products />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
         </Route>
         <Route path='/register' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
