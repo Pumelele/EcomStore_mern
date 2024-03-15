746689809d9187b02f0ac030f7b00471a2485432
Pumelele Qata <71076398+Pumelele@users.noreply.github.com>
2024-03-15 14:12:22 +0200

    more edits on cartPage

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -192,7 +192,7 @@ const CartPage = () => {
                   <div className="col-md-4">
                     <p>{p.name}</p>
                     <p>{p.description.substring(0, 30)}</p>
-                    <p>Price : R{p.price}</p>
+                    {/* <p>Price : R{p.price}</p> */}
                   </div>
                   <div className="col-md-4 cart-remove-btn">
                     <button
@@ -229,7 +229,7 @@ const CartPage = () => {
               <h2>Cart Summary</h2>
               {/* <p>Total | Checkout | Payment</p> */}
               <hr />
-              <h4>Total : {totalPrice()} </h4>
+              <h4>Total : {cart.length} Items</h4>
               {/* {auth?.user?.address ? (
                 <>
                   <div className="mb-3">
